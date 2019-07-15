399504266b9cd72ad993bdb53a7568024756064d
disolaterx <kartikyadav@protonmail.com>
2019-07-16 00:08:21 +0530

    testing

--- routes/diet.js
+++ routes/diet.js
@@ -7,8 +7,8 @@ router.use(bodyParser.json());
 /* GET home page. */
 router.get("/dontdo", function(req, res, next) {
   console.log(req.query);
-  var createNew = `DROP TABLE useraccount`
-  // var createNew = "CREATE TABLE fityouaccount(uid VARCHAR PRIMARY KEY,name VARCHAR,email VARCHAR (355) UNIQUE ,created_on TIMESTAMP NOT NULL,last_login TIMESTAMP);"
+  // var createNew = `DROP TABLE useraccount`
+  var createNew = "CREATE TABLE fityouaccount(uid VARCHAR PRIMARY KEY,name VARCHAR,email VARCHAR (355) UNIQUE ,created_on TIMESTAMP NOT NULL,last_login TIMESTAMP);"
   db.query(createNew, (err, data) => {
     console.log(data);
     console.log(err);
