db70c1d588a04c80296ec8c609aba005b54877d5
disolaterx <kartikyadav@protonmail.com>
2019-07-15 23:43:37 +0530

    testing

--- routes/users.js
+++ routes/users.js
@@ -12,18 +12,23 @@ router.get("/", function(req, res, next) {
     db.query(sql, (err, data) => {
       if(err !== null){
         res.json("error")
+        console.log("in error");
+        
       }
       else {
         if(data.rowCount > 0){
-          res.json(data.rows)
+          console.log(data.rows);
         }
         else {
           var createNew = `INSERT INTO fityouaccount (uid,fullname,email,created_on,last_login) VALUES (${req.uid},${req.name},${req.email},NOW(),NOW());`
           db.query(createNew, (err, data) => {
             if(err !== null){
               res.json("erro")
+              console.log('inner error');
+              
             }
             else {
+              console.log(data.rows);
               res.json(data)
             }
           })
