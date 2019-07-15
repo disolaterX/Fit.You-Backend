01b1ad3c1fc5c07f76cd6fd7be4358ea8ffef4ef
disolaterx <kartikyadav@protonmail.com>
2019-07-15 23:57:04 +0530

    testing

--- routes/users.js
+++ routes/users.js
@@ -8,37 +8,16 @@ router.use(bodyParser.json());
 router.get("/", function(req, res, next) {
   console.log(req.query);
   if(req.query.uid&&req.query.name && req.query.email){
-    var sql = `SELECT * FROM fityouaccount WHERE email = ${req.query.email}`;
-    db.query(sql, (err, data) => {
-      if(err !== null){
-        res.json(err)
-        console.log("in error");
-        
-      }
-      else {
-        if(data.rowCount > 0){
-          console.log(data.rows);
-        }
-        else {
+ 
           var createNew = `INSERT INTO fityouaccount (uid,fullname,email,created_on,last_login) VALUES (${req.uid},${req.name},${req.email},NOW(),NOW());`
           db.query(createNew, (err, data) => {
-            if(err !== null){
-              res.json("erro")
-              console.log('inner error');
-              
-            }
-            else {
-              console.log(data.rows);
-              res.json(data)
-            }
+           console.log(err);
+           console.log(data);
+           
+           
           })
         }
-      }
-     
-     
-    })
-  }
- 
+    
 });
 router.get("/retrive/:id", function(req, res, next) {
   console.log(req.params.id);
